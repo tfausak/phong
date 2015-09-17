399159bca5dcdc73c7b43a5f44a878bdbf289db7
Taylor Fausak <taylor@fausak.me>
2015-09-17 08:10:03 -0500

    Implement a "game" that does nothing

--- library/Phong.hs
+++ library/Phong.hs
@@ -1,4 +1,40 @@
 module Phong where
 
+import qualified Graphics.Gloss.Interface.Pure.Game as Gloss
+
 main :: IO ()
-main = return ()
+main = Gloss.play
+    displayMode
+    backgroundColor
+    stepRate
+    initialWorld
+    renderWorld
+    handleEvent
+    handleStep
+
+displayMode :: Gloss.Display
+displayMode = Gloss.FullScreen (1440, 900)
+
+backgroundColor :: Gloss.Color
+backgroundColor = Gloss.makeColor 0.5 0.5 0.5 0.0
+
+stepRate :: Int
+stepRate = 1
+
+initialWorld :: World
+initialWorld = World
+    {
+    }
+
+renderWorld :: World -> Gloss.Picture
+renderWorld world = Gloss.Text (show world)
+
+handleEvent :: Gloss.Event -> World -> World
+handleEvent _ world = world
+
+handleStep :: Float -> World -> World
+handleStep _ world = world
+
+data World = World
+    {
+    } deriving (Eq, Ord, Read, Show)

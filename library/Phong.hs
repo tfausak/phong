5b44e232bf925421d0f23eb7bbab8331dfb22124
Taylor Fausak <taylor@fausak.me>
2015-09-17 08:44:13 -0500

    Move the ball each step

--- library/Phong.hs
+++ library/Phong.hs
@@ -26,6 +26,7 @@ stepRate = 1
 initialWorld :: World
 initialWorld = World
     { ballPosition = (0.0, 0.0)
+    , ballVelocity = (-2.0, -1.0)
     }
 
 renderWorld :: World -> Gloss.Picture
@@ -39,10 +40,19 @@ handleEvent :: Gloss.Event -> World -> World
 handleEvent _ world = world
 
 handleStep :: Float -> World -> World
-handleStep _ world = world
+handleStep time world =
+    let (px, py) = ballPosition world
+        (vx, vy) = ballVelocity world
+        px' = px + time * vx
+        py' = py + time * vy
+    in  world
+        { ballPosition = (px', py')
+        , ballVelocity = (vx, vy)
+        }
 
 data World = World
     { ballPosition :: (Float, Float)
+    , ballVelocity :: (Float, Float)
     } deriving (Eq, Ord, Read, Show)
 
 ballRadius :: Float
